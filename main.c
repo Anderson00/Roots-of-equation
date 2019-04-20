ae3900b8f60ca1df12f9f6acdb555d25a08a5f6c
Anderson <anderson.hld@outlook.com>
2019-04-20 16:22:21 -0300

    Gerador de csv
    Refinamento do programa

--- main.c
+++ main.c
@@ -10,9 +10,61 @@ char *funcNames[] = {"x^5 - 3x^4 - 3x^2 + 2",
                      "sqrt(x) - 5^-x",
                      "x^5 - x^4 - 4x + 1",
                      "0.05x^3 - 0.4x^2 +3*x*senx"};
-double    E[] = {2.0e-5, 10.0e-3, 0.001, 0.005};
+double E[] = {2.0e-5, 10.0e-3, 0.001, 0.005};
+
+//All fun
+void printFunctions(void);//Printa todos os calculos de todos os metodos e funcões
+void printFunctionsInCSV(void);//Printa no padrão CSV
+void GenerateCSV(const char *filename);//redireciona o stdout para o arquivo *filename
+//End
 
 int main(int argc, char **argv){  
+    //GenerateCSV("Resultados.csv");
+    //printFunctionsInCSV();
+    printFunctions();
+    return 0;
+}
+
+void GenerateCSV(const char *filename){
+    freopen(filename, "w", stdout);
+}
+
+void printFunctionsInCSV(void){
+    RaizAux aux = {};
+    aux.e = 0.005;
+    aux.interAB.a = 0.30;
+    aux.interAB.b = 3;
+    Results resultados = {};
+    
+    for(int i = 0; i < 4; i++){
+        RaizAux aux = {};
+        aux.e = E[i];
+        aux.interAB.a = 0.30;
+        aux.interAB.b = 3;
+        Results resultados = {};
+        Results resultados2 = {};
+        Results resultados3 = {};
+
+        printf(",\n");
+
+        printf("f(x) = %s, com e = %lf\n\n", funcNames[i], aux.e);
+        
+        bisection(table[i], aux, &resultados);
+        posicao_falsa(table[i], aux, &resultados2);
+        pegaso(table[i], aux, &resultados3);
+        printf(",\n");
+        printf(",Bisection,Posição Falsa,Pégaso\n");
+        printf("x,%.8lf,%.8lf,%.8lf\n", resultados.x0, resultados2.x0, resultados3.x0);
+        printf("Iterações,%d,%d,%d\n", resultados.iterations, resultados2.iterations, resultados3.iterations);
+        printf("Interv da Solução,\"(%.8lf, %.8lf)\",\"(%.8lf, %.8lf)\",\"(%.8lf, %.8lf)\"\n", 
+            resultados.solutInterval.a, resultados.solutInterval.b,
+            resultados2.solutInterval.a, resultados2.solutInterval.b,
+            resultados3.solutInterval.a, resultados3.solutInterval.b);
+        printf("Erro Absoluto,%.8lf,%.8lf,%.8lf\n\n", resultados.fx0, resultados2.fx0, resultados3.fx0);
+    }
+}
+
+void printFunctions(void){
     RaizAux aux = {};
     aux.e = 0.005;
     aux.interAB.a = 0.30;
@@ -29,7 +81,7 @@ int main(int argc, char **argv){
         printf("f(x) = %s, com e = %lf\n", funcNames[i], aux.e);
         
         bisection(table[i], aux, &resultados);
-        printf("------------------| bissection |------------------\n");
+        printf("------------------| Bisection |------------------\n");
         printf("x: \t\t\t%.8lf\n", resultados.x0);
         printf("Iterações: \t\t%d\n", resultados.iterations);
         printf("Inter da Solução: \t(%.8lf %.8lf)\n", resultados.solutInterval.a, resultados.solutInterval.b);
@@ -52,6 +104,4 @@ int main(int argc, char **argv){
         printf("Erro Absoluto: \t\t%.8lf\n", resultados.fx0);
         printf("--------------------------------------------------\n\n");
     }
-    
-    return 0;
 }
